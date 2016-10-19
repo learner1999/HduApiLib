3583a925a4821feefefe8018ba60a795e546f771
learner <roadoflearning@live.com>
2016-10-19 13:54:45 +0800

    初始化 项目

---
+++ src/cn/zheteng123/hduapilib/Main.java
@@ -0,0 +1,10 @@
+package cn.zheteng123.hduapilib;
+
+/**
+ * Created on 2016/10/19.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
